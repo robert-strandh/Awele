b23a0f43b3f012ee60541470dd122e0dafa2bbdd
Robert Strandh <robert.strandh@gmail.com>
2019-04-05 14:36:37 +0200

    Untabify for better tool support.

--- awele.lisp
+++ awele.lisp
@@ -11,11 +11,11 @@
   ((%player-1-captured :initform 0 :accessor player-1-captured)
    (%player-2-captured :initform 0 :accessor player-2-captured)
    (%houses :initform (let ((houses (loop repeat 12
-					  collect (make-instance 'house))))
-			;; Make the list circular.
-			(setf (cdr (last houses)) houses)
-			houses)
-	    :accessor houses)))
+                                          collect (make-instance 'house))))
+                        ;; Make the list circular.
+                        (setf (cdr (last houses)) houses)
+                        houses)
+            :accessor houses)))
 
 (defun new-game ()
   (make-instance 'game))
@@ -29,9 +29,9 @@
 ;;; the move.
 (defun move (game house-number)
   (let* ((rest (nthcdr house-number (houses game)))
-	 (seed-count (seed-count (car rest))))
+         (seed-count (seed-count (car rest))))
     (setf (seed-count (car rest)) 0)
     (loop for houses = (cdr rest) then (cdr houses)
-	  until (zerop seed-count)
-	  do (incf (seed-count (car houses)))
-	     (decf seed-count))))
+          until (zerop seed-count)
+          do (incf (seed-count (car houses)))
+             (decf seed-count))))
